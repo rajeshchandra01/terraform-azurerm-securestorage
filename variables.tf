179541a9487c6c8c51b73dac8b30c02d8cec1140
rajesh chandra <rajesh.chandra@live.com>
2023-04-16 09:43:42 -0400

    initial version

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  type        = string
+  description = "storage account"
+}
+variable "location" {
+  type = string
+
+}
+variable "account_name" {
+  type = string
+}
+variable "account_tier" {
+  type    = string
+  default = "Standard"
+}
+variable "account_replication_type" {
+  type    = string
+  default = "GRS"
+}
